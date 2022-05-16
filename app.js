8d378cb5277bf608a5755aed3586c17bff4cc185
keithmw <keithmw5597@yahoo.com>
2022-05-15 20:04:01 -0500

    generate webpage

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    //Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-};
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-printProfileData(profileDataArgs);
+    console.log('Porfolio complete!  Check out index.html to see the output!');
+});
+
+
